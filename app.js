c494b3f65dacab2bd47901c27794bce5aaba0de7
mmaquer2 <mike.maquera212@gmail.com>
2020-12-25 17:14:15 -0600

    adding images

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const search_btn = document.querySelector("#btn");
 search_btn.addEventListener("click", get_data)
 
     
-//get the id of the result field
+//get the id of the result fields
+
+drinkPhoto_field = document.querySelector('#drink_photo')
 drinkName_field = document.querySelector('#res_name')
+drinkIngred_field = docum.querySelector('#res_ingreds')
 drinkInstr_field = document.querySelector('#res_instr')
+
 
 function get_data(){
     var input_data = document.getElementById('search').value;
@@ -28,22 +32,47 @@ async function search_drinks(input_data) {
     process_output(data)
 }
 
-//process the data from the api
+//process the data from the api request
 function process_output(data){
     const drinkName = data.drinks[0].strDrink
     const drinkInstr = data.drinks[0].strInstructions
-    const result_list = [drinkName,drinkInstr]
+    const drink_image = data.drinks[0].strDrinkThumb
+    const ingred_List = split_ingreds(data)
+    
+    const result_list = [drinkName,drinkInstr,ingred_List,drink_image]
+   
     display_output(result_list)
 }
+
+//iterate through each of the ingredient steps
+function split_ingreds(data) {
+    ingred_list = []
+    //loop through each of the ingredients in the object
+    //stop the loop if there value is null
+    
+    data.drinks[0].strIngredient+i
+
+    return ingred_list
+}
+
 
 
 //display result of the processed data
 function display_output(result_list){
     
     //image of the drink
+    drinkPhoto_field.innerHTML = result_list[2]
+
+    //drink name
     drinkName_field.innerHTML = result_list[0]
-    drinkInstr_field.innerHTML = result_list[1]
+
     //ingredient list
+    drinkIngred_field.innerHTML = result_[3]
+
+    //instructions 
+    drinkInstr_field.innerHTML = result_list[1]
+
+    
 
 }
 
